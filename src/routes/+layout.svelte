75a07f66a8cedc554d6ecddd6b2daeda7c37ed45
samiMHMD <s.mhamdi10@gmail.com>
2024-03-11 17:27:48 +0100

    New routes for DG/Trans choice and graphName choice

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '@fortawesome/fontawesome-free/css/all.min.css'
 	import OptionsPanel from '$lib/optionsPanel.svelte'
 	import { appName } from '$lib/generalStore'
+	import { graphName } from '$lib/generalStore'
 	import type { LayoutData } from './$types'
 
 	export let data: LayoutData
@@ -14,7 +15,9 @@
 
 <div class="page">
 	<header>
-		<div>Sam Graf Viewer</div>
+		<div>Sami Graf Viewer</div>
+
+		<!-- App selection -->
 		<div class="custom-select" style="width:200px;">
 			{#await data.apps}
 				<span>Waiting for the list to be downloaded</span>
@@ -27,6 +30,30 @@
 				</select>
 			{/await}
 		</div>
+
+        <!-- DataGraph or Transaction selection -->
+        <div class="custom-select" style="width:200px;">
+            <select bind:value={graphTypeSelected} name="graphTypeSelected" id="graph-type-selector">
+                <option value="default" selected>Select Graph Type</option>
+                <option value="DataGraphs">DataGraphs</option>
+                <option value="Transactions">Transactions</option>
+            </select>
+        </div>
+
+		<!-- Graph selection -->
+		<div class="custom-select" style="width:200px;">
+			{#await data.apps}
+				<span>Waiting for the list to be downloaded</span>
+			{:then apps}
+				<select bind:value={graphSelected} name="graphSelected" id="graph-selector" on:change={() => ($graphName = graphSelected)}>
+					<option value="default" selected>Select a Graph</option>
+					{#each apps as app}
+						<option value={app.graphName} selected={app.graphName == $graphName}>{app.graphName}</option>
+					{/each}
+				</select>
+			{/await}
+		</div>
+
 	</header>
 	<div class="wrapper">
 		<OptionsPanel />
@@ -34,7 +61,7 @@
 	</div>
 
 	<footer>
-		<div>Sam Graf Viewer footer</div>
+		<div>Sami Graf Viewer footer</div>
 	</footer>
 </div>
 
