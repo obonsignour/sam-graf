e388cb7ed97c447558d3d6531ef740285620b235
Olivier <o.bonsignour@castsoftware.com>
2024-03-12 17:44:57 -0400

    Styling fixes

--- src/routes/Applications/[appName]/DataGraphs/+page.svelte
+++ src/routes/Applications/[appName]/DataGraphs/+page.svelte
@@ -1,39 +1,39 @@
 <script lang="ts">
-	import { appName, pageTitle } from '$lib/generalStore';
-	import type { PageData } from './$types';
-  
-	export let data: PageData;
-	$: $pageTitle = `DataGraphs for ${$appName}`
+	import { appName, pageTitle } from '$lib/generalStore'
+	import type { PageData } from './$types'
 
-  </script>
-  
-  <div class="content">
+	export let data: PageData
+	$: $pageTitle = `DataGraphs for ${$appName}`
+</script>
+
+<div class="content">
 	{#await data.datagraphs}
-	  <dialog>Loading datagraphs list</dialog>
+		<dialog>Loading datagraphs list</dialog>
 	{:then datagraphs}
-	  {#if datagraphs}
-		<div class="header" id="header_id">Datagraph Id</div>
-		<div class="header" id="header_name">Datagraph Name</div>
-		{#each datagraphs as datagraph}
-		  <div id={datagraph.id}><a href="/Applications/{$appName}/DataGraphs/{datagraph.id}">{datagraph.id}</a></div>
-		  <div id={datagraph.id}><a href="/Applications/{$appName}/DataGraphs/{datagraph.id}">{datagraph.name} </a></div>
-		{/each}
-	  {/if}
+		{#if datagraphs}
+			<div class="header" id="header_id">Datagraph Id</div>
+			<div class="header" id="header_name">Datagraph Name</div>
+			{#each datagraphs as datagraph}
+				<div id={datagraph.id}><a href="/Applications/{$appName}/DataGraphs/{datagraph.id}">{datagraph.id}</a></div>
+				<div id={datagraph.id}><a href="/Applications/{$appName}/DataGraphs/{datagraph.id}">{datagraph.name} </a></div>
+			{/each}
+		{/if}
 	{/await}
-  </div>
-  
-  <style>
+</div>
+
+<style>
 	.content {
-	  margin: 1rem;
-	  display: grid;
-	  grid-template-columns: 1fr 1fr;
-	  height: 450px;
-	  overflow-y: auto;
+		margin: 1rem;
+		display: grid;
+		grid-template-columns: 3fr 1fr;
+		height: 100%;
+		min-width: 40%;
+		max-width: 80%;
+		overflow-y: auto;
 	}
-  
+
 	.header {
-	  font-weight: 600;
-	  font-size: 1.5rem;
+		font-weight: 600;
+		font-size: 1.5rem;
 	}
-  </style>
-  
+</style>
