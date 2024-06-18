017304caa259962fe7c3b888af1a0f915a25002d
Olivier <o.bonsignour@castsoftware.com>
2024-06-17 22:38:34 -0400

    algoLaunchPad added to start the compute of the algo for a given graph with a selection of relationships types existing in the graph. Barebones done. Need to get from Sami how he is planning to launch the algo

--- src/routes/Applications/[appName]/DataGraphs/[dataGraphName]/+page.svelte
+++ src/routes/Applications/[appName]/DataGraphs/[dataGraphName]/+page.svelte
@@ -3,20 +3,27 @@
 	import Viewer from '$lib/viewer.svelte'
 	import type { PageData } from './$types'
 	import AnotherSelector from '$lib/anotherSelector/+page.svelte'
+	import AlgoLaunchPad from '$lib/algoLaunchPad.svelte'
 
 	export let data: PageData
 
 	$: $pageTitle = `DataGraph ${data.name} for ${$appName}`
 
-	let selected = new Set<string>();
+	let selected = new Set<string>()
 
 	function toggleSelection(value: string) {
 		if (selected.has(value)) {
-			selected.delete(value);
+			selected.delete(value)
 		} else {
-			selected.add(value);
+			selected.add(value)
 		}
-		$relationType = Array.from(selected).join(', ');
+		$relationType = Array.from(selected).join(', ')
+	}
+
+	function getLinkTypes() {
+		const graph = data.graph
+		if (!graph) return
+		const linkTypes = new Set<string>()
 	}
 </script>
 
@@ -26,7 +33,7 @@
 	<div>Select an application</div>
 {/if}
 
-<div class="content">
+<!-- <div class="content">
 	{#await data.relationsTypes}
 		<span>Waiting for the list to be downloaded</span>
 	{:then relationsTypes}
@@ -48,7 +55,7 @@
 			<p>No links found</p>
 		{/if}
 	{/await}
-</div>
+</div> -->
 
 <style>
 	.content {
@@ -94,8 +101,7 @@
 		margin: 0.5rem 0;
 	}
 
-	.custom-select input[type="checkbox"] {
+	.custom-select input[type='checkbox'] {
 		margin-right: 0.5rem;
 	}
 </style>
-
